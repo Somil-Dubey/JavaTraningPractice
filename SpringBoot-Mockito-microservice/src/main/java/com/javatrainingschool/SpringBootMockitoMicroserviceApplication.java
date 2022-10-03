ea3115ff2e105bbf09df13544cdcf8f24599624d
Somil-Dubey <somil.dubey@soprasteria.com>
2022-10-03 12:25:22 +0530

    Initial commit

---
+++ SpringBoot-Mockito-microservice/src/main/java/com/javatrainingschool/SpringBootMockitoMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.javatrainingschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*import org.springframework.boot.autoconfigure.SpringBootApplication;*/
+
+@SpringBootApplication
+public class SpringBootMockitoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMockitoMicroserviceApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-Mockito-microservice/src/test/java/com/javatrainingschool/BookServiceTest_Author.java
@@ -0,0 +1,42 @@
+package com.javatrainingschool;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*import com.javatrainingschool.SpringBootMockitoMicroserviceApplication;
+*/import com.javatrainingschool.entity.Book;
+import com.javatrainingschool.repository.BookRepository;
+import com.javatrainingschool.service.BookService;
+
+@SpringBootTest(classes = SpringBootMockitoMicroserviceApplication.class)
+public class BookServiceTest_Author {
+	
+	
+	@Mock
+	BookRepository bookRepo;
+
+	@InjectMocks
+	BookService bookService = new BookService();
+
+	@Test
+	public void testGetAllBooks() {
+
+		List<Book> books = new ArrayList<>();
+		Book b = new Book(1, "Learn Java", "Abc", 100, "borrowed");
+		books.add(b);
+
+		when(bookRepo.findAll()).thenReturn(books);
+		List<Book> result = bookService.getAllBooks();
+		
+		Assertions.assertNotEquals(null, result);
+		Assertions.assertTrue(result.get(0).getAuthor().endsWith("-Dubey"));
+		Assertions.assertEquals("Abc-Dubey", result.get(0).getAuthor());
+
+}}
